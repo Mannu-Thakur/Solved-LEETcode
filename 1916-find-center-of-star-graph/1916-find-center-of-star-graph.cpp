59c90660ec5f22bbb7b75ef4475a879fc36844b0
Mannu Kumar Thakur <thakurmannu656@gmail.com>
2025-07-01 00:04:16 +0530

    Time: 0 ms (100%), Space: 99.1 MB (88.81%) - LeetHub

--- 1916-find-center-of-star-graph/1916-find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/1916-find-center-of-star-graph.cpp
@@ -1,24 +1,31 @@
+// class Solution {
+// public:
+//     int findCenter(vector<vector<int>>& edges) {
+//         int cnt = INT_MIN;
+
+//         unordered_map<int, int>mpp;
+//         for(auto &f : edges){
+//             int a = f[0];
+//             int b = f[1];
+//             mpp[a]++;
+//             mpp[b]++;
+//         }
+//         int num = 0;
+
+//         for(auto &x: mpp){
+//             if(cnt < x.second){
+//                 cnt = x.second;
+//                 num = x.first;
+//             }
+//         }
+
+//         return num;
+//     }
+// };/
+
 class Solution {
 public:
-    int findCenter(vector<vector<int>>& edges) {
-        int cnt = INT_MIN;
-
-        unordered_map<int, int>mpp;
-        for(auto &f : edges){
-            int a = f[0];
-            int b = f[1];
-            mpp[a]++;
-            mpp[b]++;
-        }
-        int num = 0;
-
-        for(auto &x: mpp){
-            if(cnt < x.second){
-                cnt = x.second;
-                num = x.first;
-            }
-        }
-
-        return num;
-    }
+int findCenter(vector<vector<int>>& e) {
+    return e[0][0] == e[1][0] || e[0][0] == e[1][1] ? e[0][0] : e[0][1];
+}
 };
