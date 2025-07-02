0b2c9d40513cf6fcc232b97bd82d159b9d4bddb6
Mannu Kumar Thakur <thakurmannu656@gmail.com>
2025-07-02 15:26:37 +0530

    Time: 0 ms (100%), Space: 10.4 MB (88.7%) - LeetHub

--- 0909-stone-game/0909-stone-game.cpp
+++ 0909-stone-game/0909-stone-game.cpp
@@ -1,22 +1,7 @@
 class Solution {
 public:
-    int dp[501][501];
+   bool stoneGame(vector<int>& piles) {
+    return true;  
+}
 
-    int helper(vector<int>& piles, int i, int j) {
-        if (i > j) return 0;
-        if (dp[i][j] != -1) return dp[i][j];
-
-        // Choose left pile
-        int pickLeft = piles[i] - helper(piles, i + 1, j);
-        // Choose right pile
-        int pickRight = piles[j] - helper(piles, i, j - 1);
-
-        // Store and return the best outcome
-        return dp[i][j] = max(pickLeft, pickRight);
-    }
-
-    bool stoneGame(vector<int>& piles) {
-        memset(dp, -1, sizeof(dp));
-        return helper(piles, 0, piles.size() - 1) > 0;
-    }
 };
