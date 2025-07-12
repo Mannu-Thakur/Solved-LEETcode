a69fe6fc53bdc72e0a0943b7c42032cf620f630b
Mannu Kumar Thakur <thakurmannu656@gmail.com>
2025-07-12 10:21:25 +0530

    Time: 73 ms (39.56%), Space: 71.6 MB (69.68%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
@@ -1,40 +1,32 @@
-// 'class Solution {
-// public:
-//     long long countFairPairs(vector<int>& nums, int lower, int upper) {
-//         multiset<int> seen;
-//         long long count = 0;
-
-//         for (int x : nums) {
-//             // Count how many previous numbers y satisfy:
-//             // lower - x <= y <= upper - x
-//             auto low = seen.lower_bound(lower - x);
-//             auto high = seen.upper_bound(upper - x);
-//             count += distance(low, high);
-
-//             // Insert current number for future comparisons
-//             seen.insert(x);
-//         }
-
-//         return count;
-//     }
-// };
-// '
-
-
-
 class Solution {
 public:
+    // Binary search function to return the largest index i where nums[i] < target
+    int bin_search(vector<int>& nums, int l, int r, int target) {
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] >= target) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return r;
+    }
+
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
         sort(nums.begin(), nums.end());
-        long long count = 0;
+        long long res = 0;
         int n = nums.size();
 
         for (int i = 0; i < n; ++i) {
-            int left = lower_bound(nums.begin() + i + 1, nums.end(), lower - nums[i]) - nums.begin();
-            int right = upper_bound(nums.begin() + i + 1, nums.end(), upper - nums[i]) - nums.begin();
-            count += (right - left);
+            int low = lower - nums[i];
+            int up = upper - nums[i];
+
+            int right = bin_search(nums, i + 1, n - 1, up + 1);
+            int left = bin_search(nums, i + 1, n - 1, low);
+            res += (right - left);
         }
 
-        return count;
+        return res;
     }
 };
