7742c50e899ab4c7be32bfbecdb70443cb10439d
Mannu Kumar Thakur <thakurmannu656@gmail.com>
2025-07-16 17:38:40 +0530

    Time: 0 ms (100%), Space: 13.6 MB (89.59%) - LeetHub

---
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
@@ -0,0 +1,61 @@
+// class Solution {
+//     int solve(int start, vector<bool>&vis, vector<vector<int>>&adj, vector<vector<int>>&graph, int n){
+//         int cnt = 0;
+
+//         if(start == n-1) return 1;
+//         vis[start] = true;
+
+//         for(auto &nbr: adj[start]){
+//             if(!vis[nbr]){
+//               cnt +=  solve(nbr, vis, adj, graph, n);
+//             }
+//         }
+
+//         return cnt;
+//     }
+// public:
+//     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+//         vector<vector<int>>res;
+//         int n = graph.size();
+//         int m = graph[0].size();
+
+//         vector<vector<int>>adj(n);
+//         for(int i=0; i<n; ++i){
+//            for(auto &f: graph[i]){
+//                 adj[i].push_back(f);
+//            }
+//         }
+
+
+//         vector<bool>vis(n, false);
+//        return solve(0, vis, adj, graph, n);
+//     }
+// };
+
+
+
+
+class Solution {
+    void solve(int start, vector<int>& path, vector<vector<int>>& res, vector<vector<int>>& graph, int n) {
+        path.push_back(start);
+
+        if (start == n - 1) {
+            res.push_back(path);  // found one complete path
+        } else {
+            for (auto &nbr : graph[start]) {
+                solve(nbr, path, res, graph, n);
+            }
+        }
+
+        path.pop_back();  // backtrack
+    }
+
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<vector<int>> res;
+        vector<int> path;
+        solve(0, path, res, graph, n);
+        return res;
+    }
+};
