7151a5057f08f64a14f65a3c05547db4d0eef9fb
Mannu Kumar Thakur <thakurmannu656@gmail.com>
2025-07-18 08:07:40 +0530

    Time: 3 ms (64.11%), Space: 17.2 MB (53.1%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+        unordered_map<int, int> points;
+        int max_num = 0;
+        
+        for(int num : nums){
+            points[num] += num;
+            max_num = max(max_num, num);
+        }
+        
+        int pick = 0, not_pick = 0;
+        
+        for(int i = 0; i <= max_num; ++i){
+            int curr_pick = not_pick + points[i];   
+            int curr_not_pick = max(pick, not_pick);   
+            
+            pick = curr_pick;
+            not_pick = curr_not_pick;
+        }
+        
+        return max(pick, not_pick);
+    }
+};
