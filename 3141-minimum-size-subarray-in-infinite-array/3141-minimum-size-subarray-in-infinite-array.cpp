201b95d855b3ae9b16934d3a447ba065ef388bc8
Mannu Kumar Thakur <thakurmannu656@gmail.com>
2025-07-24 09:55:16 +0530

    Time: 3 ms (92.92%), Space: 56.6 MB (95.13%) - LeetHub

--- 3141-minimum-size-subarray-in-infinite-array/3141-minimum-size-subarray-in-infinite-array.cpp
+++ 3141-minimum-size-subarray-in-infinite-array/3141-minimum-size-subarray-in-infinite-array.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
     int minSizeSubarray(vector<int>& nums, int target) {
-        long long total = accumulate(nums.begin(), nums.end(), 0LL), sum = 0;
-        int n = nums.size(), partial = n, complete = n * (target / total), start = 0;
-        target %= total;
-        for (int end = 0; end < 2 * n; end++) {
+        int n = nums.size();
+        long long totalSum = accumulate(nums.begin(), nums.end(), 0LL);
+
+         
+        int fullRepeats = target / totalSum;
+        int remainingTarget = target % totalSum;
+ 
+        if (remainingTarget == 0) return fullRepeats * n;
+  
+ 
+        int minLen = INT_MAX;
+        int start = 0;
+        long long sum = 0;
+
+        for (int end = 0; end < 2 * n; ++end) {
             sum += nums[end % n];
-            while (sum > target) {
-                sum -= nums[start++ % n];
+
+            while (sum > remainingTarget && start <= end) {
+                sum -= nums[start % n];
+                ++start;
             }
-            if (sum == target) {
-                partial = min(partial, end - start + 1);
+
+            if (sum == remainingTarget) {
+                minLen = min(minLen, end - start + 1);
             }
         }
-        return partial != n ? partial + complete : -1;
+
+        return (minLen == INT_MAX) ? -1 : minLen + fullRepeats * n;
     }
 };
