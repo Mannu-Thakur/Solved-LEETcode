6304712c7845f9f812fadc828a87fd3cee355666
Mannu Kumar Thakur <thakurmannu656@gmail.com>
2025-08-03 07:47:51 +0530

    Time: 12 ms (59.1%), Space: 20.7 MB (86.53%) - LeetHub

---
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int solve(int n, int m, vector<vector<int>>& matrix, int i, int j, vector<vector<int>>& dp) {
+        if (i >= n || j >= m) {
+            return 0; 
+        }
+
+        if (dp[i][j] != -1) { 
+            return dp[i][j];
+        }
+
+        int len = 1; // Initialize with 1 as the current cell itself contributes to the length
+
+        int dx[] = {-1, 1, 0, 0}; // Directions: up, down, left, right
+        int dy[] = {0, 0, -1, 1};
+
+        for (int k = 0; k < 4; k++) {
+            int newX = i + dx[k];
+            int newY = j + dy[k];
+
+            if (newX >= 0 && newY >= 0 && newX < n && newY < m && matrix[newX][newY] > matrix[i][j]) {
+                len = max(len, 1 + solve(n, m, matrix, newX, newY, dp)); 
+            }
+        }
+
+        dp[i][j] = len; 
+        return len;
+    }
+
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>> dp(n, vector<int>(m, -1)); // Initialize DP array with -1
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                ans = max(ans, solve(n, m, matrix, i, j, dp));
+            }
+        }
+
+        return ans;
+    }
+};
