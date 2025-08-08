bc55606a32c2e384061a6fac292d107e7e9c25ca
Mannu Kumar Thakur <thakurmannu656@gmail.com>
2025-08-08 21:54:27 +0530

    Time: 0 ms (100%), Space: 8 MB (97.07%) - LeetHub

---
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        int n = s.length();
+        int m = goal.length();
+        if( n!= m) return false;
+
+        string newS = s + s;
+        return (newS.find(goal) < newS.size());
+    }
+};
