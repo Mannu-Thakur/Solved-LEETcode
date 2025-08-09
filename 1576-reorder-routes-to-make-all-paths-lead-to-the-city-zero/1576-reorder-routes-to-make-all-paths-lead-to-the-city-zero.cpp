7be9d0c22215b072af669ec8c2b5913c166c3a7b
Mannu Kumar Thakur <thakurmannu656@gmail.com>
2025-08-09 11:31:42 +0530

    Time: 83 ms (92.68%), Space: 106.2 MB (78.74%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,33 +1,26 @@
 class Solution {
-    void dfs(int &ans , int src, vector<int>&visited, vector<vector<int>>&forward, vector<vector<int>>&backward, int n, vector<vector<int>>&connections){
-       visited[src]=1;
-       for(auto &find: forward[src]){
-        if(!visited[find]){
-            ans++;
-            dfs(ans, find, visited, forward, backward, n, connections);
-        }
-       }
+    int solve(vector<vector<pair<int,int>>>& adj, int root, vector<bool>& vis) {
+        int cnt = 0;
+        vis[root] = true;
 
-
-       for(auto &findd: backward[src]){
-        if(!visited[findd]){
-            dfs(ans, findd, visited, forward, backward, n, connections);
+        for (auto &[nbr, dir] : adj[root]) {
+            if (!vis[nbr]) {
+                if (dir == 1) cnt++;  
+                cnt += solve(adj, nbr, vis);
+            }
         }
-       }
+        return cnt;
     }
 public:
-    int minReorder(int n, vector<vector<int>>& connections) {
-        vector<vector<int>> forward(n), backward(n);
-        for(int i=0; i<connections.size(); ++i){
-            int a = connections[i][0];
-            int b = connections[i][1];
-
-            forward[a].push_back(b);
-            backward[b].push_back(a);
+    int minReorder(int n, vector<vector<int>>& cun) {
+        vector<vector<pair<int,int>>> adj(n);
+        for (auto &f : cun) {
+            int a = f[0], b = f[1];
+            adj[a].push_back({b, 1});  
+            adj[b].push_back({a, 0});  
         }
-        vector<int>visited(n);
-        int ans = 0;
-        dfs(ans, 0, visited, forward, backward,n,connections);
-        return ans;
+        
+        vector<bool> vis(n, false);
+        return solve(adj, 0, vis);
     }
 };
