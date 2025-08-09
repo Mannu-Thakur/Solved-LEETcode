f289cbcc60e012ad9ea7ed96c679c8691bb68947
Mannu Kumar Thakur <thakurmannu656@gmail.com>
2025-08-09 17:22:46 +0530

    Time: 11 ms (10%), Space: 33.6 MB (5.02%) - LeetHub

--- 1130-last-stone-weight-ii/1130-last-stone-weight-ii.cpp
+++ 1130-last-stone-weight-ii/1130-last-stone-weight-ii.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
-        int lastStoneWeightII(vector<int> A) {
-        bitset<1501> dp = {1};
-        int sumA = 0;
-        for (int a : A) {
-            sumA += a;
-            for (int i = min(1500, sumA); i >= a; --i)
-                dp[i] = dp[i] + dp[i - a];
-        }
-        for (int i = sumA / 2; i >= 0; --i)
-            if (dp[i]) return sumA - i - i;
-        return 0;
-    }
+	int f(int i,int val,vector<int>& st,int n,vector<vector<int>>& dp){ 
+		if(i==n){
+			if(val<0) return 1e9;
+			return 0;
+		}
+		if(dp[i][val+3000]!=-1) return dp[i][val+3000];
+		int pos= st[i]+f(i+1,val+st[i],st,n,dp);
+		int neg= -st[i]+f(i+1,val-st[i],st,n,dp);
+		return dp[i][val+3000]=min(pos,neg);
+	}
+
+	int lastStoneWeightII(vector<int>& st) {
+		int n=st.size();
+		vector<vector<int>> dp(n,vector<int>(6000,-1));
+		return f(0,0,st,n,dp);
+	}
 };
+	
