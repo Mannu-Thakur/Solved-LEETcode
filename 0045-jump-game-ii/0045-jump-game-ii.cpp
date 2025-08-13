20e71117356a354c1f22aa69e6cb51e9a43fa2d7
Mannu Kumar Thakur <thakurmannu656@gmail.com>
2025-08-13 17:49:28 +0530

    Time: 0 ms (100%), Space: 20.4 MB (84.63%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,19 +1,22 @@
 class Solution {
         int find(vector<int>&nums, int n, int jumps, int ind){
             if(ind >= n-1) return jumps;
+
             int l=0;
             int r=0;
             int far = 0;
-            while( r < n-1){
-            for(int i=l; i<=r;++i){
-                far=max(far, i+nums[i]);
-            }
+
+            while( r < n - 1 ){
+             for(int i=l; i<=r; ++i){
+                far = max(far, i + nums[i]);
+             }
            
             l=r+1;
             r=far;
-            jumps++;
-            
+            jumps++;  
         }
+
+
         return jumps;
         }
 public:
