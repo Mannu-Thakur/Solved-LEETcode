670204cdf803e95f3179c818c28de51d69924ad9
Mannu Kumar Thakur <thakurmannu656@gmail.com>
2025-08-14 12:19:26 +0530

    Time: 3 ms (12.19%), Space: 10.1 MB (7.62%) - LeetHub

--- 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.cpp
@@ -4,13 +4,28 @@ public:
         string ans = "";
         int n = num.size();
 
-        for (int i = 0; i <= n - 3; i++) {
-            if (num[i] == num[i+1] && num[i] == num[i+2]) {
+        unordered_map<char, int> mp;
+        int i = 0, j = 0;
+
+        while (j < n) {
+            mp[num[j]]++;
+
+             if (j - i + 1 > 3) {
+                mp[num[i]]--;
+                if (mp[num[i]] == 0) {
+                    mp.erase(num[i]);
+                }
+                i++;
+            }
+
+             if (j - i + 1 == 3 && mp.size() == 1) {
                 string temp = num.substr(i, 3);
                 if (temp > ans) {
-                    ans = temp;  
+                    ans = temp;
                 }
             }
+
+            j++;
         }
         return ans;
     }
