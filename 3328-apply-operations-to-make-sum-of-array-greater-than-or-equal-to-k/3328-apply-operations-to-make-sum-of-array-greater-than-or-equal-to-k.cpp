22586e89a68d8034b7a357b549f553643afa973b
Mannu Kumar Thakur <thakurmannu656@gmail.com>
2025-08-16 08:06:10 +0530

    Time: 4 ms (19.09%), Space: 8.4 MB (32.27%) - LeetHub

--- 3328-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/3328-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k.cpp
+++ 3328-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/3328-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k.cpp
@@ -1,9 +1,16 @@
-class Solution
-{
+class Solution {
 public:
-    int minOperations(int k)
-    {
-        int x = sqrt(k);
-        return (x - 1 + (k - 1) / x);
+    int minOperations(int k) {
+        int minOps = INT_MAX;
+        if(k == 1) return 0;
+
+        for(int a=0; a<k; ++a){
+            int say = a+1;
+            int b = (k +say - 1)/say - 1;
+            minOps = min(minOps, a + b);
+        }
+        
+        return minOps;
+        
     }
 };
