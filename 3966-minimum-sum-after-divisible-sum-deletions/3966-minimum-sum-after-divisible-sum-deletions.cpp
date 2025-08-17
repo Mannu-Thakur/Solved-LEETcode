f5b6f559d86a0538d763bf630908147c31e2b9bc
Mannu Kumar Thakur <thakurmannu656@gmail.com>
2025-08-17 12:08:38 +0530

    Time: 321 ms (36.57%), Space: 318.2 MB (56.63%) - LeetHub

---
+++ 3966-minimum-sum-after-divisible-sum-deletions/3966-minimum-sum-after-divisible-sum-deletions.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long minArraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+
+         unordered_map<int, int> indexMap{{0, n}}; 
+
+         vector<long long> dp(n + 1, 0);
+
+        int sum = 0;
+
+         for (int i = n - 1; i >= 0; i--) {
+            sum = (sum + nums[i]) % k;
+            if (sum < 0) sum += k;
+
+             dp[i] = nums[i] + dp[i + 1]; 
+
+             if (indexMap.count(sum)) {
+                dp[i] = min(dp[i], dp[indexMap[sum]]);
+            }
+
+             indexMap[sum] = i;
+        }
+
+        return dp[0];
+    }
+};
