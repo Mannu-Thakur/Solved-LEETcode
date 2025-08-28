d4d64fb4f764a47694985f3ca94045391e95b5a9
Mannu Kumar Thakur <thakurmannu656@gmail.com>
2025-08-28 11:14:44 +0530

    Time: 157 ms (33.56%), Space: 26.1 MB (54.82%) - LeetHub

---
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    typedef pair<char, int> p;
+
+   struct cmp {
+    bool operator()(const p &a, const p &b) const {
+        if (a.first == b.first) 
+            return a.second < b.second; 
+        return a.first > b.first; 
+    }
+};
+
+
+public:
+    string clearStars(string s) {
+        int n = s.length();
+        priority_queue<p, vector<p>, cmp> pq;
+
+        for (int i = 0; i < n; ++i) {
+            if (s[i] != '*') {
+                pq.push({s[i], i});
+            } else {
+                if (!pq.empty()) {
+                    int ind = pq.top().second;
+                    pq.pop();
+                    s[ind] = '*';  
+                }
+            }
+        }
+
+        string ans = "";
+        for (int i = 0; i < n; ++i) {
+            if (s[i] != '*') ans += s[i];
+        }
+
+        return ans;
+    }
+};
