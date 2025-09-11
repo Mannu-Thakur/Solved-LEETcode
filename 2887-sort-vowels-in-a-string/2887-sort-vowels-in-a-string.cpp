948693a8902fcfdc1e74869f4d0992ffe9b13aa6
Mannu Kumar Thakur <thakurmannu656@gmail.com>
2025-09-11 16:16:38 +0530

    Time: 17 ms (43.62%), Space: 16.1 MB (68.85%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
@@ -1,58 +1,30 @@
-// class Solution {
-//     bool isVowel(char c) {
-//     return (c == 'a' || c == 'e' || c == 'i' || 
-//             c == 'o' || c == 'u' || 
-//             c == 'A' || c == 'E' || c == 'I' || 
-//             c == 'O' || c == 'U');
-// }
-
-// public:
-//     string sortVowels(string s) {
-//         int n = s.length();
-//         vector<int>res(n);
-
-
-//         for(int i=0; i<n-1; ++i){
-//             if(!isVowel(s[i]) && (s[i] - '0') < (s[i+1] < '0')){
-//                 res.push_back(s[i]);
-//             }else{
-//                 res[i] = s[i];
-//             }
-//         }
-
-//         string ans = "";
-//         for(auto &f: res){
-//             ans += f;
-//         }
-
-//         return ans;
-//     }
-// };
-
 class Solution {
     bool isVowel(char c) {
-        return (c == 'a' || c == 'e' || c == 'i' || 
-                c == 'o' || c == 'u' || 
-                c == 'A' || c == 'E' || c == 'I' || 
-                c == 'O' || c == 'U');
+        return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
     }
 
 public:
     string sortVowels(string s) {
-        vector<char> vowels;
+        string ans = "";
+        int n = s.length();
 
-         
-        for (char c : s) {
-            if (isVowel(c)) vowels.push_back(c);
+        vector<char>vowels;
+        for(auto &f: s){
+            if(isVowel(f)){
+                vowels.push_back(f);
+            }
         }
+
         sort(vowels.begin(), vowels.end());
+
         int j = 0;
-        for (int i = 0; i < s.size(); i++) {
-            if (isVowel(s[i])) {
+        for(int i=0; i<n; ++i){
+            if(isVowel(s[i])){
                 s[i] = vowels[j++];
             }
         }
 
         return s;
+
     }
 };
