f4167983e6d6e7fc4e75cdd1e671ccd7a9ee0ac0
Mannu Kumar Thakur <thakurmannu656@gmail.com>
2025-09-25 09:17:48 +0530

    Time: 0 ms (100%), Space: 12.8 MB (72.83%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,18 +1,15 @@
 class Solution {
-    int solve(vector<vector<int>>& arr, int n, int ind, int col, vector<vector<int>>& dp) {
-        if (ind == n - 1) return arr[ind][col];
-
-        if (dp[ind][col] != -1) return dp[ind][col];
-
-        int a = solve(arr, n, ind + 1, col, dp);
-        int b = solve(arr, n, ind + 1, col + 1, dp);
-
-        return dp[ind][col] = arr[ind][col] + min(a, b);
-    }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        return solve(triangle, n, 0, 0, dp);
+        vector<int> dp = triangle[n - 1]; // Start with the bottom row
+
+        for (int i = n - 2; i >= 0; --i) {
+            for (int j = 0; j <= i; ++j) {
+                dp[j] = triangle[i][j] + min(dp[j], dp[j + 1]);
+            }
+        }
+
+        return dp[0]; // Top element will hold the minimum path sum
     }
 };
