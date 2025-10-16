eebc526e8bf20bd982ec150ff221f17b999a237f
Mannu Kumar Thakur <thakurmannu656@gmail.com>
2025-10-16 08:22:57 +0530

    Time: 18 ms (24.28%), Space: 40.5 MB (48.4%) - LeetHub

--- 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.cpp
+++ 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.cpp
@@ -1,27 +1,26 @@
- 
-
 class Solution {
+    int solve(vector<int>& nums, int start, int k) {
+        int n = nums.size();
+        if (start + k > n) return -1;
+
+        for (int i = start + 1; i < start + k; ++i) {
+            if (nums[i] <= nums[i - 1]) return -1;
+        }
+        return start + k - 1;
+    }
+
 public:
     bool hasIncreasingSubarrays(vector<int>& nums, int k) {
         int n = nums.size();
-        int cur = 1, prev = 0;
+        if (k == 1) return true;
 
-        for (int i = 1; i < n; ++i) {
-            if (nums[i] > nums[i - 1]) {
-                cur++;
-            } else {
-                // check before resetting
-                if (min(prev, cur) >= k || cur / 2 >= k)
-                    return true;
-                prev = cur;
-                cur = 1;
+        for (int i = 0; i + 2 * k <= n; ++i) {
+            int end1 = solve(nums, i, k);
+            if (end1 != -1) {
+                int end2 = solve(nums, end1 + 1, k);
+                if (end2 != -1) return true;
             }
         }
-
-        // final check at the end
-        if (min(prev, cur) >= k || cur / 2 >= k)
-            return true;
-
         return false;
     }
 };
