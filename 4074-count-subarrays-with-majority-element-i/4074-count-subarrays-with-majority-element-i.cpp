a6af13c011bb7fbec39eef92cc38f7fc1a8e967d
Mannu Kumar Thakur <thakurmannu656@gmail.com>
2025-11-08 22:30:55 +0530

    Time: 32 ms (72.22%), Space: 42.1 MB (16.67%) - LeetHub

--- 4074-count-subarrays-with-majority-element-i/4074-count-subarrays-with-majority-element-i.cpp
+++ 4074-count-subarrays-with-majority-element-i/4074-count-subarrays-with-majority-element-i.cpp
@@ -8,8 +8,7 @@ public:
         for(int i=0;i<n;i++){
             a[i] = (nums[i] == target ? 1 : -1);
         }
-
-        // prefix sum
+ 
         vector<int> pref(n+1, 0);
         for(int i=0;i<n;i++){
             pref[i+1] = pref[i] + a[i];
@@ -18,7 +17,6 @@ public:
         int ans = 0;
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
-                // sum a[i..j] = pref[j+1]-pref[i]
                 if(pref[j+1]-pref[i] > 0) ans++;
             }
         }
