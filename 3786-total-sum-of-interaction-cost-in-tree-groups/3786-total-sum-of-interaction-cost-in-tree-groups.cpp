50778ec2d020521a53062dac7b36d0af5ebc6e07
Mannu Kumar Thakur <thakurmannu656@gmail.com>
2025-12-22 21:15:32 +0530

    Time: 453 ms (41.67%), Space: 444.6 MB (16.67%) - LeetHub

--- 3786-total-sum-of-interaction-cost-in-tree-groups/3786-total-sum-of-interaction-cost-in-tree-groups.cpp
+++ 3786-total-sum-of-interaction-cost-in-tree-groups/3786-total-sum-of-interaction-cost-in-tree-groups.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
+    vector<vector<int>> adj;
+    vector<int> total;
+    long long ans = 0;
+
+    vector<int> dfs(int u, int parent, vector<int>& group) {
+         vector<int> cnt(21, 0);
+
+         cnt[group[u]] = 1;
+
+        for (int v : adj[u]) {
+            if (v == parent) continue;
+
+            vector<int> child = dfs(v, u, group);
+
+             for (int g = 1; g <= 20; g++) {
+                ans += 1LL * child[g] * (total[g] - child[g]);
+                cnt[g] += child[g];
+            }
+        }
+        return cnt;
+    }
+
     long long interactionCosts(int n, vector<vector<int>>& edges, vector<int>& group) {
-        vector<vector<int>> adj(n);
+        adj.resize(n);
+        total.assign(21, 0);
+
         for (auto &e : edges) {
             adj[e[0]].push_back(e[1]);
             adj[e[1]].push_back(e[0]);
         }
 
-        // total[g] = total nodes having group g
-        vector<int> total(21, 0);
         for (int g : group) total[g]++;
 
-        long long ans = 0;
-
-        function<vector<int>(int, int)> dfs = [&](int u, int parent) {
-            vector<int> cnt(21, 0);
-            cnt[group[u]] = 1;
-
-            for (int v : adj[u]) {
-                if (v == parent) continue;
-
-                vector<int> child = dfs(v, u);
-
-                // edge (u - v) contribution
-                for (int g = 1; g <= 20; g++) {
-                    ans += 1LL * child[g] * (total[g] - child[g]);
-                    cnt[g] += child[g];
-                }
-            }
-            return cnt;
-        };
-
-        dfs(0, -1);
+        dfs(0, -1, group);
         return ans;
     }
 };
